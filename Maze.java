03657f4cc6c24da671f838e87692ac19ed73cfa4
Lachlan Murray <19769390@student.curtin.edu.au>
2020-04-01 11:07:45 +0800

    Edited maze layout a second time.

--- Maze.java
+++ Maze.java
@@ -17,8 +17,8 @@ public class Maze
     {
         int[][] grid = {
             { WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL, WALL},
-            {START, EMPTY,  WALL,  WALL, WALL, EMPTY, EMPTY, EMPTY,  WALL, WALL},
-            { WALL, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, EMPTY,  WALL, WALL},
+            {START, EMPTY,  EMPTY,  WALL, WALL, EMPTY, EMPTY, EMPTY,  WALL, WALL},
+            { WALL, WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, EMPTY,  WALL, WALL},
             { WALL,  WALL,  WALL,  WALL, EMPTY,  WALL, EMPTY,  WALL, EMPTY, WALL},
             { WALL, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, EMPTY, EMPTY, EMPTY, WALL},
             { WALL,  WALL, EMPTY,  WALL,  WALL, EMPTY, EMPTY,  WALL, EMPTY, WALL},
